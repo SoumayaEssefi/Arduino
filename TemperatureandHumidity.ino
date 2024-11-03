4f16ce00e56fcc18d624cbfb2aeb9f0cb67234d1
SoumayaEssefi <96314077+SoumayaEssefi@users.noreply.github.com>
2024-11-03 22:35:35 +0100

    Add files via upload

---
+++ TemperatureandHumidity.ino
@@ -0,0 +1,40 @@
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+#include <DHT.h>
+
+#define DHTPIN 6        
+#define DHTTYPE DHT22   
+
+DHT dht(DHTPIN, DHTTYPE);
+
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+void setup() {
+  Serial.begin(9600);
+  
+  dht.begin();
+  lcd.begin(16,2);
+  lcd.backlight();
+  lcd.setCursor(0, 0);
+  lcd.print("Temp & Humidity");
+}
+
+void loop() {
+  int humidity = dht.readHumidity();
+  int temperature = dht.readTemperature();
+  Serial.print("Humidity: ");
+  Serial.print(humidity);
+  Serial.print(" %\t");
+  Serial.print("Temperature: ");
+  Serial.print(temperature);
+  Serial.println("C");
+
+  lcd.setCursor(0, 1);
+  lcd.print("Temp:");
+  lcd.print(temperature );
+  lcd.setCursor(9, 1);
+  lcd.print("Hum:");
+  lcd.print(humidity);
+
+  delay(2000);
+}
